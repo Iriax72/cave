b86c5c3344d6645d2cf9deb45565b33fa1b74237
flexult <touma.leandre.72@gmail.com>
2025-11-30 08:25:33 +0100

    redimentions

--- js/Player.js
+++ js/Player.js
@@ -5,6 +5,10 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         scene.add.existing(this);
         scene.physics.add.existing(this);
 
+        this.setOrigin(0.5, 0.5);
+        this.setScale(0.4);
+        this.body.setSize(this.width, this.height, true);
+
         //this.setCollideWorldBounds(true);
         //this.setBounce(0.1);
         alert(this)
